e2381b360bc411b68a7ce65c11f8455cdc8e40a7
unknown <yeehawbw@gmail.com>
2020-05-01 16:46:32 -0400

    Plug in compromise nlp w/html

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
+  window.nlp.extend(window.compromiseOutput);
+
   let input = document.querySelector(".proseinput");
   let button = document.querySelector(".parsebutton");
 
   const parseFunction = function (e) {
 
-    debugger;
-    let text = input.value;
+    let text = nlp(input.value);
+    let tagged = text.html({
+      '#Noun': "keyword noun",
+      '#Verb': "keyword verb",
+      '#Adjective': 'keyword adjective',
+      '#Adverb': 'keyword adverb'
+    })
+    input.value = "";
 
     const textElement = document.createElement('p');
-    textElement.innerText = text;
+    textElement.innerHTML = tagged;
     document.body.appendChild(textElement);
   }
 
