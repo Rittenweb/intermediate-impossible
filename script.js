2eed8e454288fc63cfbcd8bb6eabec0db8cfad6d
unknown <yeehawbw@gmail.com>
2020-05-02 10:13:56 -0400

    Integrate datamuse API and interpret nouns

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-  window.nlp.extend(window.compromiseOutput);
+  const comp = window.nlp.extend(window.compromiseOutput);
 
-  let input = document.querySelector(".proseinput");
-  let button = document.querySelector(".parsebutton");
+  const input = document.querySelector(".proseinput");
+  const button = document.querySelector(".parsebutton");
+  let alternates = {};
 
   const parseFunction = function (e) {
 
-    let text = nlp(input.value);
+    let text = comp(input.value);
     let tagged = text.html({
-      '#Noun': "keyword noun",
-      '#Verb': "keyword verb",
+      '(#Noun && !#Pronoun)': "keyword noun",
+      '(#Verb && !#Copula && !#Modal && !PhrasalVerb)': "keyword verb",
       '#Adjective': 'keyword adjective',
       '#Adverb': 'keyword adverb'
     })
@@ -19,6 +20,49 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const textElement = document.createElement('p');
     textElement.innerHTML = tagged;
     document.body.appendChild(textElement);
+    let keywords = [...document.querySelectorAll(".keyword")];
+    keywords.forEach((keyword) => {
+      let keywordText = keyword.innerText.replace(/[^a-zA-Z\s]/g, "");
+      alternates[keyword.innerText] = [];
+      let tag;
+      let verbType;
+      let nounPlural = false;
+      let classList = keyword.classList;
+      if (classList.contains('noun')) {
+        tag = 'n';
+        let noun = comp(keywordText)
+        if (noun.nouns().isPlural().out('array').length) {
+          nounPlural = true;
+          keywordText = noun.nouns().toSingular().text();
+        }
+      } else if (classList.contains('verb')) {
+        tag = 'v';
+      } else if (classList.contains('adjective')) {
+        tag = 'adj';
+      } else if (classList.contains('adverb')) {
+        tag = 'adv';
+      } else {
+        return;
+      }
+      fetch(`https://api.datamuse.com/words?ml=${keywordText}`, {
+          cache: 'no-cache',
+        })
+        .then((data) => {
+          return data.json();
+        })
+        .then((data) => {
+          data.forEach((alternate) => {
+            if (alternate.tags && alternate.tags.includes(`${tag}`)) {
+              let result = alternate.word;
+              if (nounPlural) {
+                result = comp(result).nouns().toPlural().text();
+              }
+              alternates[keyword.innerText].push(result);
+            }
+          });
+        })
+    })
+    console.log(alternates);
   }
 
   button.addEventListener("click", parseFunction)
